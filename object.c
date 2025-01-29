deb3f4304e3c266d0bb98ed4c2d10358a267b24a
JP! <joaobianchi@live.com>
2025-01-29 08:57:48 +0000

    :construction: concatenating string & freeing memory

--- object.c
+++ object.c
@@ -14,6 +14,9 @@ static Obj *allocateObject(size_t size, ObjType type)
     Obj *object = (Obj *)reallocate(NULL, 0, size);
     object->type = type;
 
+    object->next = vm.objects;
+    vm.objects = object;
+
     return object;
 }
 
@@ -26,6 +29,11 @@ static ObjString *allocateString(char *chars, int length)
     return string;
 }
 
+ObjString *takeString(char *chars, int length)
+{
+    return allocateString(chars, length);
+}
+
 ObjString *copyString(const char *chars, int length)
 {
     char *heapChars = ALLOCATE(char, length + 1);
@@ -33,4 +41,14 @@ ObjString *copyString(const char *chars, int length)
     heapChars[length] = '\0';
 
     return allocateString(heapChars, length);
+}
+
+void printObject(Value value)
+{
+    switch (OBJ_TYPE(value))
+    {
+    case OBJ_STRING:
+        printf("%s", AS_CSTRING(value));
+        break;
+    }
 }
--- object.h
+++ object.h
@@ -19,6 +19,7 @@ typedef enum
 struct Obj
 {
     ObjType type;
+    struct Obj *next;
 };
 
 struct ObjString
@@ -29,6 +30,8 @@ struct ObjString
 };
 
 ObjString *takeString(char *chars, int length);
+ObjString *copyString(const char *chars, int length);
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type)
 {
